c03aaa4cafe10c77210153099ceef3d322275d0e
alexnakov <alex.nakov3@gmail.com>
2024-08-30 11:49:11 +0100

    attached loc to btn

--- main.js
+++ main.js
@@ -1,16 +1,39 @@
 document.body.style.border = "5px dashed purple";
 
 setTimeout(() => {
-  changeStatus()
-}, 3000)
+  const locations = getLocations()
+  changeStatus(locations)
+}, 5000)
 
-function changeStatus() {
+function changeStatus(locations) {
   const statuses = document.querySelectorAll(`td.status`)
-  for (let status of statuses) {
-    status.children[0].remove()
-    status.children[0].remove()
+  for (let i = 0; i < statuses.length; i++) {
+    const status = statuses[i]
+    const location = locations[i]
+    while (status.firstChild) {
+      status.children[0].remove()
+    }
     const btn = document.createElement('button')
+    btn.setAttribute('data-location', String(location))
     btn.innerText = '£?'
     status.appendChild(btn)
-  } 
+  }
+  // for (let status of statuses) {
+  //   while (status.firstChild) {
+  //     status.children[0].remove()
+  //   }
+  //   const btn = document.createElement('button')
+  //   btn.setAttribute('data-location', )
+  //   btn.innerText = '£?'
+  //   status.appendChild(btn)
+  // } 
+}
+
+function getLocations() {
+  const locationsSpans = document.querySelectorAll(`span.location`)
+  const locations = []
+  for (let location of locationsSpans) {
+    locations.push(location.textContent)
+  }
+  return locations
 }
